52b77012ac166913720b0ff9cfb72fe9b97dd2a5
Maaz-21 <155450768+Maaz-21@users.noreply.github.com>
2026-01-12 01:18:12 +0530

    Update page.js

--- frontend/app/(public)/item/[id]/page.js
+++ frontend/app/(public)/item/[id]/page.js
@@ -61,7 +61,6 @@ export default function ItemPage({ params }) {
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
-      <Navbar />
       <main className="flex-grow max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 w-full">
          <ItemDetails item={item} />
       </main>
