4697f3a18f46525cd83e3a38572dc01e0b125d5f
Khatik Maazahmed Naeemuddin <155450768+Maaz-21@users.noreply.github.com>
2026-02-21 17:19:12 +0530

    saveing

--- frontend/components/ui/Hero.js
+++ frontend/components/ui/Hero.js
@@ -26,7 +26,7 @@ export default function Hero() {
                   <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-indigo-400 opacity-75"></span>
                   <span className="relative inline-flex rounded-full h-3 w-3 bg-indigo-500"></span>
                 </span>
-                #1 Marketplace For Scrap
+                #1 Marketplace
               </motion.div>
 
               <motion.h1 
